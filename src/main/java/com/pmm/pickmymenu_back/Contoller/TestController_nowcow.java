01cdb21e387bf75f62602a3bf22b9850786eab79
nowcow <hhjnn92@icloud.com>
2024-12-20 14:41:36 +0900

    React 연동

--- src/main/java/com/pmm/pickmymenu_back/Contoller/TestController_nowcow.java
+++ src/main/java/com/pmm/pickmymenu_back/Contoller/TestController_nowcow.java
@@ -1,4 +1,13 @@
-package com.pmm.pickmymenu_back.Contoller;
+package com.pmm.pickmymenu_back.contoller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController_nowcow {
+
+    @GetMapping("/api/hello")
+    public String hello() {
+        return "Hello!";
+    }
 }
