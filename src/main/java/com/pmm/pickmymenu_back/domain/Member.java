b1cfaf0f313e65b50d2198aaf9063a603c5d5ce7
taisooqq <cookehwk@gmail.com>
2024-12-27 14:38:46 +0900

    add dependency h2 #2

--- src/main/java/com/pmm/pickmymenu_back/domain/Member.java
+++ src/main/java/com/pmm/pickmymenu_back/domain/Member.java
@@ -1,2 +1,56 @@
-package com.pmm.pickmymenu_back.domain;public class Member {
+package com.pmm.pickmymenu_back.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends TimeEntity{
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String name;
+//    @Column(nullable = false)
+//    @Enumerated(EnumType.STRING)
+//    private MemberType type;
+    @Column
+    private String refreshToken;
+
+//    @OneToMany(mappedBy = "member")
+//    private List<RoomMember> roomMemberList = new ArrayList<>();
+
+//    @Builder
+//    public Member(Long id, String email, String password, String name, MemberType type, String refreshToken) {
+//        this.id = id;
+//        this.email = email;
+//        this.password = password;
+//        this.name = name;
+//        this.type = type;
+//        this.refreshToken = refreshToken;
+//    }
+
+//    public static Member createMember(SignupRequest req){
+//        return Member.builder()
+//            .email(req.getEmail())
+//            .password(req.getPassword())
+//            .name(req.getName())
+//            .type(MemberType.USER)
+//            .build();
+//    }
+
+
 }
