80ee83fb40a8a8ee24c71c9829722596324b0b61
nowcow <hhjnn92@icloud.com>
2025-01-09 18:12:02 +0900

    readme update

--- src/main/java/com/pmm/pickmymenu_back/controller/SurveyController.java
+++ src/main/java/com/pmm/pickmymenu_back/controller/SurveyController.java
@@ -4,10 +4,9 @@ import com.pmm.pickmymenu_back.dto.BaseResponse;
 import com.pmm.pickmymenu_back.dto.request.survey.SurveyCollectReq;
 import com.pmm.pickmymenu_back.service.SurveyService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,4 +22,11 @@ public class SurveyController {
         boolean result = surveyService.collect(req);
         return BaseResponse.success(result);
     }
+
+    @GetMapping("/parentCount")
+    public List<Object[]> getParentFoodTreeCategoryCount() {
+        return surveyService.getParentFoodTreeCategoryCount();
+    }
+
+
 }
